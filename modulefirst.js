ab1673f88110eb32c17a4e45cd551a08539f10c9
keshav31 <keshavchauhangtx2004@gmail.com>
2025-01-14 18:46:09 +0530

    commonModule in nodejs

---
+++ modulefirst.js
@@ -0,0 +1,5 @@
+// import {simple as simple2} from "./modulesecond.mjs"
+import * as a2 from "./modulesecond.mjs"
+// const simple =require("./modulesecond");
+// simple2()
+console.log(a2.simple2(),a2.simple())
---
+++ modulesecond.mjs
@@ -0,0 +1,7 @@
+export function simple(){
+    console.log("simple is Complex")
+}
+export function simple2(){
+    console.log("simple2 is Complex")
+}
+// module.exports = simple;
